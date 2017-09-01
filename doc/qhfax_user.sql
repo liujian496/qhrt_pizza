3fe98d29476807bac035bf420c4abe1d83634325
root <server@qhfax.com.cn>
2017-09-01 20:09:35 +0800

    remove qhfax pizza

--- doc/qhfax_user.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `qhfax_user` (
-	`userid` varchar(32) DEFAULT NULL,
-	`username` varchar(32) DEFAULT NULL,
-	KEY `index_userid` (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='前海融通用户表'
